1b6ec204ee8d3d4f35477ea9d819499723ac6722
nabla_2 <nabla_2@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-25 07:10:59 +0000

    First import
    git-svn-id: https://plugins.svn.wordpress.org/excel-to-table/trunk@495033 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ excel-2-table.php
@@ -0,0 +1,134 @@
+<?php
+/*
+ Plugin Name: Sport Championship
+ Plugin URI: N/A
+ Description: Plugin per la visualizzazione di campionati e di calendari;
+ Version: 0.1
+ Author: Miro Barsocchi
+ Author URI: http://www.mirobarsa.com
+ */
+require_once 'excel_reader2.php';
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+define("DEFAULT_TEMP_FILE",'wp-content'.DIRECTORY_SEPARATOR.'plugins'.DIRECTORY_SEPARATOR.'sport-championship'.DIRECTORY_SEPARATOR.'excel_file');
+
+
+function TableDatas($file) {
+	$data = new Spreadsheet_Excel_Reader($file);
+	$html = $data->dump_clean(true,true);
+	$html .= "<p>";
+	$html .= $data->dump_clean(true,true,1);
+	return $html;
+}
+
+
+
+function championship_modify() {
+	if(isset($_POST['deletecheck'])) {
+		$filesToDelete = $_POST['deletecheck'];
+		foreach($filesToDelete as $filename) {
+			$realNameFile ="..".DIRECTORY_SEPARATOR.DEFAULT_TEMP_FILE.DIRECTORY_SEPARATOR.$filename; 
+			if(file_exists($realNameFile)){
+				unlink($realNameFile);
+			}
+		}
+	}
+	for ($countFiles = 0; $countFiles < count ($_FILES['upload']['name']); $countFiles++) {
+			if($_FILES['upload']['name'][$countFiles] != "") {
+				$uploadFileName=$_FILES['upload']['name'][$countFiles];
+				$uploadFileNameExtension =substr($uploadFileName,strlen($uploadFileName)-4,strlen($uploadFileName));
+				if ($uploadFileNameExtension == ".xls"){
+					$excelFileDirectory = "..".DIRECTORY_SEPARATOR.DEFAULT_TEMP_FILE.DIRECTORY_SEPARATOR.$uploadFileName;
+					if (!move_uploaded_file ($_FILES['upload']['tmp_name'][$countFiles],$excelFileDirectory)) {
+						echo "<p>".$uploadFileName.": Errore di caricamento";
+					}
+				}else {
+					echo "<p>".$uploadFileName.": Formato non riconosciuto!<p>";
+				}
+			}
+	}
+	
+?>
+<div class="wrap">
+<div id='icon-options-general' class='icon32'><br />
+</div>
+
+<h2>Modifica calendario</h2>
+<br/>
+<br/>
+</div>
+<form id="champ-settings" enctype="multipart/form-data" action="" method="post">
+<table  class="widefat">
+  			<thead>
+  				<tr>
+					<th scope="col" width="15%">Elimina</th>
+					<th scope="col" >Nome file</th>
+				</tr>
+  			</thead>
+  			<tbody>
+			<?php 
+			$excels = glob("..".DIRECTORY_SEPARATOR.DEFAULT_TEMP_FILE.DIRECTORY_SEPARATOR."*.xls");
+			if ($excels) {
+				$cont =0;
+				foreach($excels as $excel) {
+					$nameExcel = basename($excel , ".xls");
+					$linkForDownload = plugins_url().'/sport-championship/excel_file/'.$nameExcel.'.xls';
+					$class = ('alternate' != $class) ? 'alternate' : ''; ?>
+				    <tr>
+						<th scope="row" class="check-column">
+						<input type="checkbox" name="deletecheck[<?php echo $cont; ?>]" value="<?php echo $nameExcel.".xls";?>"/></th>
+						<td><a href="<?php echo $linkForDownload;?>"><?php echo $nameExcel;?></a></td>
+					</tr>
+	 			<?php $cont ++;} ?>
+	 		<?php } else { ?>
+				<tr id='no-id'><td scope="row" colspan="5"><em>Nessun campionato</em></td></tr>
+			<?php 
+			}
+		?>
+			
+			
+			</tbody>
+		</table>
+		aggiungi/aggiorna (puoi selezionare anche pi&ugrave; file) <input type="file" name="upload[]" multiple>
+		<br>
+<input type="submit" class="button-secondary action" id="doaction" name="doaction" value="Esegui"/>
+</form>
+<?php
+}
+
+function show_championship_submenu() {
+	if(function_exists('add_submenu_page')) {
+		add_object_page('championship','championship',0,'championship-mb','championship_modify');
+	}
+}
+
+function show_championship_table() {
+	global $wp_query;
+	$excels = glob(DEFAULT_TEMP_FILE.DIRECTORY_SEPARATOR."*.xls");
+	if (!isset($wp_query->query_vars['girn'])) {
+		$cont=1;
+		foreach ($excels as $excel) {
+			$link = basename($excel , ".xls");
+			$result = get_permalink();
+			$params = array( 'girn' => $cont );
+			$result = add_query_arg( $params, $result );
+			$output .= "<a href=\"".$result ."\" >".$link."</a>
+			<p>";
+			$cont++;
+		}
+	}else {
+		$intIndex =intval($wp_query->query_vars['girn']);
+		$intIndex -= 1;
+		$output = TableDatas($excels[$intIndex]);
+	}
+	return $output;
+}
+
+function parameter_queryvars_for_me( $qvars ){
+	$qvars[] = 'girn';
+	return $qvars;
+}
+
+add_filter('query_vars', 'parameter_queryvars_for_me' );
+add_shortcode( 'championship_table','show_championship_table');
+add_action('admin_menu','show_championship_submenu');
+?>
---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Plugin Name ===
+Contributors: nabla
+Donate link: http://example.com/
+Tags: table, excel, office, convert
+Requires at least: 2.0.2
+Tested up to: 3.3.1
+Stable tag: 1.0
+
+Convert Excel files to html standard tabel
+
+== Description ==
+
+Based on code from  php Excel Reader at http://code.google.com/p/php-excel-reader/ this simple 
+plugin convert all excel files in a specified folder in html table.
+
+It simply create a list of link of Excel file in a page where you put the tag 
+
+[excel_table] and when you open one of it you could see a page with the table
+
+== Installation ==
+
+To get this plugin working 
+
+e.g.
+
+1. Upload  folder `excel-2-table` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place Excel files in folder 'excel_file' inside path
+1. Place `[excel_table]` in your templates
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+
+1. Manager panel
+2. link of files in post
+3. View of example table
+
+== Changelog ==
+
+= 1.0 =
+First release
+
+== Upgrade Notice ==
+
+= 1.0 =
+First release
